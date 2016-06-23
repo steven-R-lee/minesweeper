fa8c2ec3c00e93b3ac6353b9d2fd83042612978f
steven-R-lee <srleeforero@gmail.com>
2016-06-23 13:14:05 +1200

    third thru fifth edits of minesweeper game

--- minesweeper.js
+++ minesweeper.js
@@ -1,14 +1,24 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
+// declare global variables
+var board = {
+  cells:[]
+};
+
 function startGame () {
   var kids = document.getElementsByClassName('board')[0].children;
-  addListeners(kids);
+
+  for (var n = 0; n < kids.length; n++) {
+    addListeners(kids[n]);
+    addCellToBoard(kids[n]);
+  }
 }
 
+
 function addListeners(element) {
-  for (var n = 0; n < element.length; n++) {
-    element[n].addEventListener('click', showCell);
-    element[n].addEventListener('contextmenu', markCell);
+  for (var m = 0; m < element.length; m++) {
+    element[m].addEventListener('click', showCell);
+    element[m].addEventListener('contextmenu', markCell);
   }
 }
 
@@ -19,4 +29,29 @@ function showCell(evt) {
 function markCell(evt) {
   evt.preventDefault();
   evt.target.classList.toggle('marked');
+}
+
+function getRow(element) {
+  var list = element.classList;
+    for (var n = 0; n < list.length; n++) {
+      if ((list[n].contains('row-')) && (list[n].contains('col-'))) {
+        return (parseInt(list[n].charAt(4)));
+   }
+ }
+}
+
+function getCol(element) {
+  var list = element.classList;
+    for (var n = 0; n < list.length; n++) {
+      if ((list[n].contains('row-')) && (list[n].contains('col-'))) {
+         return (parseInt(list[n].charAt(10)));
+   }
+ }
+}
+
+function addCellToBoard(element) {
+  var newCell = {};
+  newCell.row = getRow(element);
+  newCell.col = getCol(element);
+  newCell.isMine = element.classList.contains('mine');
 }
