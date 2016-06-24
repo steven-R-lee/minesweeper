c92805f27806e25c462b92e407d6f992876739cd
steven-R-lee <srleeforero@gmail.com>
2016-06-24 12:48:08 +1200

    sixth edit of minesweeper game

--- minesweeper.js
+++ minesweeper.js
@@ -12,6 +12,10 @@ function startGame () {
     addListeners(kids[n]);
     addCellToBoard(kids[n]);
   }
+
+  for (var n = 0; n < board.cells.length; n++) {
+    board.cells[n].surroundingMines = countMines(board.cells[n]);
+  }
 }
 
 
@@ -56,4 +60,15 @@ function addCellToBoard(element) {
     newCell.isMine = element.classList.contains('mine');
 
 board.cells.push(newCell);
+}
+
+function countMines(cell) {
+  var surroundingCells = getSurroundingCells(cell.row, cell.col);
+  var count = 0;
+    for (var m = 0; m < surroundingCells.length; m++) {
+      if(surroundingCells[m].isMine) {
+        count = count + 1;
+      }
+    }
+    return count;
 }
